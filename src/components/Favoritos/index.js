71dc7d315bc1be586859d4ad22bbb8a7fe2b0ad0
David T. <davidt.info@gmail.com>
2025-04-01 20:54:14 -0300

    Desafio: construindo páginas

--- src/components/Favoritos/index.js
+++ src/components/Favoritos/index.js
@@ -1,8 +1,19 @@
-import styles from './Favoritos.module'
+import Banner from 'components/Banner';
+import styles from './Favoritos.module.css'
+import Titulo from 'components/Titulo';
+import videos from 'json/db.json'
+import Card from 'components/Card';
 
 function Favoritos() {
     return (
         <>
+            <Banner imagem="favoritos"></Banner>
+            <Titulo>Meus Favoritos</Titulo>
+            <section className={styles.container}>
+            <Card id='2' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
+            {/* <Card id='2' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png'} />            */}
+            
+            </section>
         </>
     )
 }
