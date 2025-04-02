ff3358ca6982ce4ff5e0e77352127ba7c2c50de4
David T. <davidt.info@gmail.com>
2025-04-01 21:00:38 -0300

    Resolução do desafio

--- src/components/Favoritos/index.js
+++ src/components/Favoritos/index.js
@@ -1,7 +1,6 @@
 import Banner from 'components/Banner';
 import styles from './Favoritos.module.css'
 import Titulo from 'components/Titulo';
-import videos from 'json/db.json'
 import Card from 'components/Card';
 
 function Favoritos() {
@@ -10,9 +9,7 @@ function Favoritos() {
             <Banner imagem="favoritos"></Banner>
             <Titulo>Meus Favoritos</Titulo>
             <section className={styles.container}>
-            <Card id='2' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
-            {/* <Card id='2' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png'} />            */}
-            
+                <Card id='2' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
             </section>
         </>
     )
