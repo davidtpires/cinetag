8de5453f834cb4a98e020a27aca0f3f6d617e2a3
David T. <davidt.info@gmail.com>
2025-04-04 10:03:39 -0300

    useEffect

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,20 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from 'json/db.json'
 import styles from './Inicio.module.css'
+import { useEffect, useState } from "react";
 
 function Inicio() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/davidtpires/cinetag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
     return (
         <>
             <Banner imagem="home" />
@@ -13,11 +23,11 @@ function Inicio() {
             </Titulo>
             <section className={styles.container}>
                 {videos.map((video) => {
-                   return <Card {...video} key={video.id} />
+                    return <Card {...video} key={video.id} />
                 })}
             </section>
         </>
-        
+
     )
 }
 
